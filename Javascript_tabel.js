0bc1c152b5351ab88c13e8f4f79288df437677fe
Apin70 <alfianbagus70@gmail.com>
2021-01-14 17:00:02 +0700

    NGasih fitur geser gambar

---
+++ Javascript_tabel.js
@@ -0,0 +1,11 @@
+function formtext(tugas) {
+var Username =(document.tugas.Username.value);
+var Email =(document.tugas.Email.value);
+var Password =(document.tugas.Password.value);
+var Phonenumber = document.tugas.Phonenumber.value;
+
+document.tugas.Usernameout.value = Username ;
+document.tugas.Emailout.value = Email ;
+document.tugas.Passwordout.value = Password;
+document.tugas.Phonenumberout.value = Phonenumber;
+}
